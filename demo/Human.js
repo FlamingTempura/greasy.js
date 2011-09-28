0fdf2535f2bb95522e1d87491cf38ccbce6af0b0
Peter West <Peter West>
2011-09-28 01:33:19 +0100

    Changed README. Fixed typo in Human.js

--- demo/Human.js
+++ demo/Human.js
@@ -10,9 +10,9 @@ window.animalLibrary.greasy.defineComponent(
             greasy = imports.greasy,
             Thumb = greasy.get("Thumb"),
             thumb1 = Thumb.create({ name: "Thumb 1" }),
-            thumb2 = Thumb.create({ name: "Thumb 2" });
+            thumb2 = Thumb.create({ name: "Thumb 2returngith
 
-        return {
+        " }); {
             greeting: function () {
                 return "Hello World!<br />" + thumb1.greeting() +
                     "<br />" + thumb2.greeting();
